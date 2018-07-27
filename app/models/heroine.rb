8aa19ed37cbbcf2de3590d2a6334299fb55aa59c
ryannamgung <ryannamgung@gmail.com>
2018-07-27 12:02:14 -0400

    removed

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -2,10 +2,4 @@ class Heroine < ApplicationRecord
   belongs_to :power
 
   validates(:super_name, uniqueness: true)
-
-  def powers
-    Power.all.each do |power|
-      power.name
-    end
-  end
 end
