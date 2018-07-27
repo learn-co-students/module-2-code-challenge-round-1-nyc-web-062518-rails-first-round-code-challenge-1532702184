c54ca8cf80a73a70ff2d30df1a3b2ccd47e271cf
ryannamgung <ryannamgung@gmail.com>
2018-07-27 11:58:13 -0400

    done with no bonus :)

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,11 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates(:super_name, uniqueness: true)
+
+  def powers
+    Power.all.each do |power|
+      power.name
+    end
+  end
 end
