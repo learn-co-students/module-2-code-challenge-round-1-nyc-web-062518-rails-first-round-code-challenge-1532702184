2da792f309dae9776968b944cf25825683dd18d8
seanraad123 <seanconrad123@gmail.com>
2018-07-27 12:07:36 -0400

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,49 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: [:show, :edit, :update, :destroy]
+
   def index
     @heroines = Heroine.all
+
+    # BONUS STUFF
+    if params[:q]
+      @heroine = Heroine.search(params[:search]).order("created_at DESC")
+    else
+      @heroine = Heroine.all.order("created_at DESC")
+    end
+    # END BONUS STUFF
+
   end
+
+  def show
+    render :show
+  end
+
+  def new
+    @heroine = Heroine.new
+    render :new
+  end
+
+  def create
+    @heroine = Heroine.new(set_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+
+    # THIS IS THE STRONG PARAMS TO TRY AND ALLOW THE SEARCH THROUGH
+    # params.require(:heroine).permit(:name, :super_name, :power_id, :q)
+  end
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:show]
+  resources :heroines, only: [:new, :create, :index, :show]
 end
