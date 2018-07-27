b42ac485593766e0039a06ff6626dd78954b6797
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-07-27 12:08:56 -0400

    mod 2 challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,37 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      flash[:success] = "Good luck on your super adventure!"
+      redirect_to @heroine
+    else
+      flash[:error] = @heroine.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    @heroine = Heroine.find(params[:id])
+    @heroine.destroy
+    flash[:destroy] = "Your Super Heroine has been deleted."
+    redirect_to heroines_path
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
