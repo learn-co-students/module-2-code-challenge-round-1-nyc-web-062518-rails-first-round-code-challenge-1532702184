bd4f7a40a683319aa2fe97c713aca0132e4bf9be
jaredianmills <jaredianmills@gmail.com>
2018-07-27 12:08:29 -0400

    Done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,17 @@
+#Had extra time, so for the show page for each power, I added links to each Heroine who has that power.
+
 class PowersController < ApplicationController
+
+  before_action :set_power, only: [:show]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+  def set_power
+    @power = Power.find_by(id: params[:id])
   end
 end
