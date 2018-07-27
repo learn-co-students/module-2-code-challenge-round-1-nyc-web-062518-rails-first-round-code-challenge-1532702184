67c860958d06d8aca460c1425c246100062ae2e2
github username <github email address>
2018-07-27 11:43:01 -0400

    met all deliverables plus bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,29 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    if params[:power]
+      @power = Power.find_by(name: params[:power])
+      @heroines = Heroine.where('power_id LIKE ?', "#{@power.id}")
+    else
+      @heroines = Heroine.all
+    end
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(params.require(:heroine).permit(:name, :super_name, :power_id, :power))
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :name, :super_name, presence: true
+  validates :super_name, uniqueness: { case_sensitive: false }
 end
